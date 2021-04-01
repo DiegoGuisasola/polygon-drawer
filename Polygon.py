8dea883a3eb91c1d5d9e8f76f08bcba705fddccd
Diego Guisasola <diego.guisasola@utp.edu.co>
2021-04-01 09:30:05 -0500

    Added error handler for size < 3

--- Polygon.py
+++ Polygon.py
@@ -2,8 +2,18 @@ import turtle
 import time
 
 class Polygon:
-    def __init__(self, side, name, linecolor='black', linewidth=1, stepsize=100, speed=3):
-        self.side = side
+    def __init__(self, side, name='non-set', linecolor='black', linewidth=1, stepsize=100, speed=3):
+        try:
+            if side < 3:
+                self.side = -1
+                raise ValueError
+            else:
+                self.side = side
+        except:
+            print('ValueError: The amount of sides must be greater than 2')
+            
+        
+        
         self.name = name
         self.angle = (self.side-2)*180/self.side
         self.linecolor = linecolor
@@ -16,6 +26,9 @@ class Polygon:
         return self.name
 
     def draw(self):
+
+        if self.side == -1: # Handles error: self.side < 3
+            return
     # Initialize everything
         # turtle.color(self.color)
         turtle.getscreen()
@@ -29,11 +42,8 @@ class Polygon:
             t.left(180-self.angle)
         
         time.sleep(1)
-
         t.clear()
 
-
-        
 
 class Triangle(Polygon):
     def __init__(self, linecolor='black', linewidth=1):
@@ -48,15 +58,24 @@ class Pentagon(Polygon):
         super().__init__(5,'pentagon', linecolor, linewidth)
 
 class Circle(Polygon):
-    def __init__(self, linecolor='black', linewidth=1, stepsize=5, speed='fastest'):
-        super().__init__(100,'circle', linecolor, linewidth,stepsize,speed)
+    def __init__(self, radius = 50, linecolor='black', linewidth=1, stepsize=5, speed='fastest'):
+        super().__init__(1, 'circle', linecolor, linewidth,stepsize,speed)
+        self.radius = radius
+    
+    def draw(self):
+        t = turtle.Turtle()
+        t.circle(self.radius)
 
 
-p1 = Triangle()
-p1.draw()
+def draw_example():
+    p1 = Triangle('red')
+    p1.draw()
 
-p2 = Square()
-p2.draw()
+    p2 = Square('red')
+    p2.draw()
 
-p3 = Circle('red')
-p3.draw()
+    p3 = Circle(linecolor='red')
+    p3.draw()
+
+if __name__ == '__main__':
+    draw_example()
